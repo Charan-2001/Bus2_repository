4e4a2448604a880737bcf85dbc347294579e4178
Charan-2001 <79699471+Charan-2001@users.noreply.github.com>
2023-03-06 12:18:35 +0530

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -1,12 +1,12 @@
-import math 
+import math
 
 def test_sqrt():
-num = 25
-assert math.sqrt(num) == 5
+	num=25
+	assert math.sqrt(num)==5
 
-def test_sqrt():
-num = 7
-assert 7*7 == 49
+def testsquare():
+	num=7
+	assert 7*7==49
 
 def testequality():
-assert 10 == 10
+	assert 10==10
